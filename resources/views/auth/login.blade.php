7788e43bcb13528c750b7ca06c67531f794996ee
softcommteam2 <softcommteam2@gmail.com>
2022-11-25 09:18:55 +0630

    lkk

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,10 +71,6 @@
     </div>
 </div>
 @endsection --}}
-
-
-
-
 <!DOCTYPE html>
 
 
@@ -116,10 +112,10 @@
                       </div>
                       <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Enter your email address" value="{{ old('email') }}" required autocomplete="email" autofocus>
                       @error('email')
-                                         <span class="invalid-feedback" role="alert">
-                                             <strong>{{ $message }}</strong>
-                                         </span>
-                                     @enderror
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
                     </div>
                     <div class="input-group mb-4">
                       <div class="input-group-prepend">
@@ -128,12 +124,11 @@
                         </span>
                       </div>
                       <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Enter your password" required autocomplete="current-password">
-
-                                     @error('password')
-                                         <span class="invalid-feedback" role="alert">
-                                             <strong>{{ $message }}</strong>
-                                         </span>
-                                     @enderror
+                          @error('password')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
                     </div>
                     <div class="row">
                       <div class="col-6">
